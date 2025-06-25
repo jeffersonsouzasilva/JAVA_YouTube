72444d0de6eac876aa793ef1f0dfdb3d2738bd12
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2025-06-24 21:01:02 -0300

    Fernanda Kipper - Curso de PROGRAMAÇÃO JAVA para INICIANTES | Fundamentos + Programação Orientada Objetos -- Tipos primitivos > momento 23:33

--- @KipperDev/Curso de PROGRAMAÇÃO JAVA para INICIANTES/java-curso/src/Main.java
+++ @KipperDev/Curso de PROGRAMAÇÃO JAVA para INICIANTES/java-curso/src/Main.java
@@ -3,6 +3,32 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+/*      //numeros inteiros = 100, 234345, 255, 120, 1,9193939393939394
+        byte => 8 bits => -128 a 127
+        short => 16 bits => -13.768 a 32.767
+        int => 32 bits => -2.147.483.648 a 2.147.483.647
+        long => 64 bits => -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+        // numeros decimais = 1.2, 3.4, 5.6, 7.8, 9.0
+        // 5.50
+        float => 32 bits => precisão simples
+        double => 64 bits => precisão dupla
+
+        String => representar palavras e frases
+        char => representar um unico caractere => a,b,c,d
+        char meuChar = 'a';
+        meuChar = "Fernanda";
+
+        boolean => true ou false
+        */
+
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 100000L;
+        float f = 10.5f;
+        double d = 20.5;
+        char c = 'A';
+        boolean bool = true;
     }
-}
+}// Declarar uma variavel
+

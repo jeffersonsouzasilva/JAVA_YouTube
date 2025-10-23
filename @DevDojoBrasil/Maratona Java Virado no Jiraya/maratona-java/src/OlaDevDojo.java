9e3b4c5fce19d916ca1c25eb95e54fe7b0d2258f
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2025-10-22 21:49:31 -0300

    Canal @DevDojoBrasil - Conhecendo a IDE Intellij Community -- Primeiro código

---
+++ @DevDojoBrasil/Maratona Java Virado no Jiraya/maratona-java/src/OlaDevDojo.java
@@ -0,0 +1,10 @@
+// Atalho psvm = public static void main(String[] args)
+// Atalho sout = System.out.println("");
+//Atalho ctrl + / = comentar a linha
+
+public class OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HA");
+
+    }
+}

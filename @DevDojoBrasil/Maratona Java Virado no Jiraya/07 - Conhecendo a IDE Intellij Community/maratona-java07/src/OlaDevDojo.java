5c418a6270acf9308a9742f943123f14b6f16a8c
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2025-10-24 16:57:59 -0300

    Canal @DevDojoBrasil - Organizando o código em pacotes

--- @DevDojoBrasil/Maratona Java Virado no Jiraya/07 - Conhecendo a IDE Intellij Community/maratona-java07/src/OlaDevDojo.java
+++ @DevDojoBrasil/Maratona Java Virado no Jiraya/07 - Conhecendo a IDE Intellij Community/maratona-java07/src/OlaDevDojo.java
@@ -5,6 +5,5 @@
 public class OlaDevDojo {
     public static void main(String[] args) {
         System.out.println("KA-ME-HA-ME-HA");
-
     }
 }
---
+++ @DevDojoBrasil/Maratona Java Virado no Jiraya/maratona-java/src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;// Atalho psvm = public static void main(String[] args)
+
+// Atalho sout = System.out.println("");
+//Atalho ctrl + / = comentar a linha
+// Atalho ctrl + f9 = build o projeto
+
+/*
+    Direito do mouse + " Reload form Disk " ( Caso não apareça a alteração feito use " Reload form Disk ")
+*/
+
+public class OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HA");
+
+    }
+}

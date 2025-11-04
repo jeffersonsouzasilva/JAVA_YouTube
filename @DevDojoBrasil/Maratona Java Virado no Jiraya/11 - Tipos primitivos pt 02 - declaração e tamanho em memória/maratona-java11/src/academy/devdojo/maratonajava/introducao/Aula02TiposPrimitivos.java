edd4864913e0c014476ee83bea667a54e2e59cd9
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2025-11-03 21:25:59 -0300

    Canal @DevDojoBrasil - Tipos primitivos pt 03 -- casting

--- @DevDojoBrasil/Maratona Java Virado no Jiraya/11 - Tipos primitivos pt 02 - declaração e tamanho em memória/maratona-java11/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ @DevDojoBrasil/Maratona Java Virado no Jiraya/11 - Tipos primitivos pt 02 - declaração e tamanho em memória/maratona-java11/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,21 +1,19 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int idade = (int) 10000000000L;
-        long numeroGrande = (long) 155.23;
-        double salarioDouble = 2000.0D;
-        float salarioFloat = (float) 2500.0D;
-        byte idadeByte = 127;
-        short idadeShort = 32000;
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = -128;
+        short idadeShort = 10;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = '\u8041';
-
         System.out.println("A idade é " + idade + " anos");
         System.out.println(falso);
         System.out.println("char "+caractere);
-        System.out.println(numeroGrande);
     }
 }

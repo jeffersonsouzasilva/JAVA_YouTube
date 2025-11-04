f0b6dd153907868bf5c22cbe7d74766f02f305ee
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2025-11-04 20:28:48 -0300

    Canal @DevDojoBrasil - Tipos primitivos pt 03 e pt 04 -- ajustar e verificar

---
+++ @DevDojoBrasil/Maratona Java Virado no Jiraya/12 - Tipos primitivos pt 03 - casting/maratona-java12/src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.introducao;
+/*
+    Atalho psvm = public static void main(String[] args)
+    Atalho sout = System.out.println("");
+    Atalho ctrl + / = comentar a linha
+    Atalho ctrl + d = clonar a linha para baixo
+    Atalho ctrl + f9 = build o projeto
+    Direito do mouse + " Reload form Disk " ( Caso não apareça a alteração feito use " Reload form Disk ")
+    Atalho ctrl + b = Para ir ao método de cada item do código ou javadoc do código( a documentação referente e explicando o significado ). Obs: clique no item do código antes de dar ctrl + b, exemplo clicar em println e dar ctrl + b
+    Atalho ctrl + q ou passar o cursor acima do código = barra de informações referente ao código
+    Atalho ctrl + shift + f10 = Executer ( Run )
+    Atalho shift + f10 = Executer ( Tentei com esse código e deu Run ) (para duas classes muda a execução entre elas)
+
+*/
+
+/**
+ * Isso é um comentário <b>javadoc</b>
+ * Toggle Rendered View via Ctrl + Alt + Q ou seja para renderizar Ctrl + Alt + Q
+ */
+public class Aula01OlaDevDojo {
+    //Isso é um Comentario em linha
+    /*
+        Isso é um comentario
+        de multiplas
+        linhas
+    */
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HA");
+
+    }
+}

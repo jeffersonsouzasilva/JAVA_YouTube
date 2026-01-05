6a1d28f8c1709b80fbb001054bd1b7a4f538d050
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-01-05 19:28:42 -0300

    Canal @LearnwithW3Schools -- W3Schools Java Full Course For Beginners -- Definindo estrutura do projeto

---
+++ @LearnwithW3Schools/Full Playlist W3Schools Java Full Course (20 Lessons)/JavaFullCourseForAbsoluteBeginners/src/com/w3schools/javafullcourseforbeginners/lesson/Lesson1.java
@@ -0,0 +1,21 @@
+package com.w3schools.javafullcourseforbeginners.lesson;
+//Java intro & Syntax
+
+/**
+ About This Lesson (Lesson-1: Java Intro & Syntax)
+ In today's lesson, we cover:
+ What is Java?
+ Java Introduction
+ How Java Works
+ Java Syntax Basics
+ Your First Java Program
+ Understanding main()
+ Printing Output in Java
+ Running Java Code
+ */
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}

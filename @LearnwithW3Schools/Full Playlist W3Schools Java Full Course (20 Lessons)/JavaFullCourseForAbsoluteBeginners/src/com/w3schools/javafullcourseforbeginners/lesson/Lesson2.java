5733644cf8514c9a50d19ffa314fbc79b262fa8e
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-01-06 18:11:37 -0300

    Canal @LearnwithW3Schools -- W3Schools Java Full Course For Beginners -- Lesson 02

---
+++ @LearnwithW3Schools/Full Playlist W3Schools Java Full Course (20 Lessons)/JavaFullCourseForAbsoluteBeginners/src/com/w3schools/javafullcourseforbeginners/lesson/Lesson2.java
@@ -0,0 +1,31 @@
+package com.w3schools.javafullcourseforbeginners.lesson;
+//Java Output
+
+/**
+ In today's lesson, we cover:
+ Java Output
+ */
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        //método println próxima linha
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+
+        System.out.println(" ");
+
+        //método print mesma linha
+        System.out.print("Hello World! ");
+        System.out.print("I will print on the same line.");
+
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+        System.out.println(3 + 3);
+        System.out.println(2 * 5);
+    }
+}

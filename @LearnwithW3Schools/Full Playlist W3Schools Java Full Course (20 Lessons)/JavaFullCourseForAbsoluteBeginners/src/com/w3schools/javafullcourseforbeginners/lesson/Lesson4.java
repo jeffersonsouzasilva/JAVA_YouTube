01508284c7f20dd61eae1169a69caf957b431215
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-01-15 19:31:33 -0300

    Canal @LearnwithW3Schools -- W3Schools Java Full Course For Beginners -- Lesson 04

--- @LearnwithW3Schools/Full Playlist W3Schools Java Full Course (20 Lessons)/JavaFullCourseForAbsoluteBeginners/src/com/w3schools/javafullcourseforbeginners/lesson/Lesson4.java
+++ @LearnwithW3Schools/Full Playlist W3Schools Java Full Course (20 Lessons)/JavaFullCourseForAbsoluteBeginners/src/com/w3schools/javafullcourseforbeginners/lesson/Lesson4.java
@@ -8,5 +8,23 @@ package com.w3schools.javafullcourseforbeginners.lesson;
 public class Lesson4 {
     public static void main(String[] args) {
 
+        int myNum = 5;                  // Integer (whole number)
+        float myFloatNum = 5.99f;       // Floating point number
+        char myLetter = 'D';            // Character
+        boolean myBool = true;          // Boolean
+        String myText = "Hello";        // String
+
+        // Create integer variables
+        int length = 4;
+        int width = 6;
+        int area;
+
+        // Calculate the area of a rectangle
+        area = length * width;
+
+        // Print variables
+        System.out.println("Length is: " + length);
+        System.out.println("Width is: " + width);
+        System.out.println("Area of the rectangle is: " + area);
     }
 }

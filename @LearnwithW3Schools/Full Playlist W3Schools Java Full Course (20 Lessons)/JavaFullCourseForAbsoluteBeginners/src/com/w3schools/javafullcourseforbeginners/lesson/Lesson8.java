969fb8c0282ee492f942b0fbd73ef6878bfce944
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-01-21 23:57:52 -0300

    Canal @LearnwithW3Schools -- W3Schools Java Full Course For Beginners -- Lesson 08

---
+++ @LearnwithW3Schools/Full Playlist W3Schools Java Full Course (20 Lessons)/JavaFullCourseForAbsoluteBeginners/src/com/w3schools/javafullcourseforbeginners/lesson/Lesson8.java
@@ -0,0 +1,21 @@
+package com.w3schools.javafullcourseforbeginners.lesson;
+
+/**
+ In today's lesson, we cover:
+ Java Math
+ */
+
+public class Lesson8 {
+    public static void main(String[] args) {
+        System.out.println(Math.max(5, 10));
+        System.out.println(Math.min(5, 10));
+
+        System.out.println(" ");
+        System.out.println(Math.sqrt(64));
+
+        System.out.println(" ");
+        int no = 64;
+        double sq = Math.sqrt(no);
+        System.out.println("The Square Root of " + no + "is : " + sq);
+    }
+}

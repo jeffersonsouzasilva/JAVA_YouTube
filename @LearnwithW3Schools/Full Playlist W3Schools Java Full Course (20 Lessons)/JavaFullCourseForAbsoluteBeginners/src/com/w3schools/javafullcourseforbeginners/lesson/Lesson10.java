1efcda34b1e7ea3e7969e7946128896d55534fd6
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-01-23 16:03:27 -0300

    Canal @LearnwithW3Schools -- W3Schools Java Full Course For Beginners -- Lesson 10

---
+++ @LearnwithW3Schools/Full Playlist W3Schools Java Full Course (20 Lessons)/JavaFullCourseForAbsoluteBeginners/src/com/w3schools/javafullcourseforbeginners/lesson/Lesson10.java
@@ -0,0 +1,30 @@
+package com.w3schools.javafullcourseforbeginners.lesson;
+
+public class Lesson10 {
+    public static void main(String[] args) {
+        if (20 > 18) {
+            System.out.println("20 is greater than 18");
+        }
+
+        System.out.println(" ");
+        int x = 20;
+        int y = 18;
+        if (x > y) {
+            System.out.println("x is greater than y");
+        }
+
+        System.out.println(" ");
+        x = 20;
+        y = 18;
+        if (x < y){
+            System.out.println("x is greater than y");
+            System.out.println("x is greater than y");
+        } //end of if
+        else {
+            System.out.println("Hey its False!!!");
+            System.out.println("x is greater than y");
+            System.out.println("x is greater than y");
+            System.out.println("x is greater than y");
+        }//end of else
+    }//end of main
+}//end of class

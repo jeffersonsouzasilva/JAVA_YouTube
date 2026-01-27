a060876b596e8d482ecd8328cda252c6d414b3a8
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-01-27 18:53:20 -0300

    Canal @LearnwithW3Schools -- W3Schools Java Full Course For Beginners -- Lesson 13

---
+++ @LearnwithW3Schools/Full Playlist W3Schools Java Full Course (20 Lessons)/JavaFullCourseForAbsoluteBeginners/src/com/w3schools/javafullcourseforbeginners/lesson/Lesson13.java
@@ -0,0 +1,67 @@
+package com.w3schools.javafullcourseforbeginners.lesson;
+
+public class Lesson13 {
+    public static void main(String[] args) {
+
+        /*
+        int day = 1;
+        switch (day) {
+            default:
+                System.out.println("Please Enter day betwen 1-7");
+                break;
+            case 1:
+                System.out.println("Monday");
+                System.out.println("Monday");
+                System.out.println("Monday");
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+         */
+        // Outputs "Thursday" (day 4)
+
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+        // Outputs "Thursday" (day 4)
+
+    }
+}

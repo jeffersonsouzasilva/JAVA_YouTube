13597f3d7419f1fe8ae832022404043baec3f90e
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-01-30 16:29:40 -0300

    Canal @LearnwithW3Schools -- W3Schools Java Full Course For Beginners -- Lesson 16

---
+++ @LearnwithW3Schools/Full Playlist W3Schools Java Full Course (20 Lessons)/JavaFullCourseForAbsoluteBeginners/src/com/w3schools/javafullcourseforbeginners/lesson/Lesson16.java
@@ -0,0 +1,30 @@
+package com.w3schools.javafullcourseforbeginners.lesson;
+
+/**
+ In today's lesson, we cover:
+ Java Nested For Loop
+ */
+
+public class Lesson16 {
+    public static void main(String[] args) {
+        // Outer loop
+        for (int i = 1; i <= 2; i++) {
+            System.out.println("Outer: " + i); // Executes 2 times
+
+            // Inner loop
+            for (int j = 1; j <= 3; j++) {
+                System.out.println(" Inner: " + j); // Executes 6 times (2 * 3)
+            }
+        } //end of Outer For Loop
+
+
+        System.out.println(" ");
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        for (String car : cars) {
+            System.out.println(car);
+        }
+
+        
+    }
+}

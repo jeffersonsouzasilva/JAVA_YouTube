bd83b0a504e7d4a283d7e6825cda2d470caa564f
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-03-21 21:26:10 -0300

    Canal @KipperDev -- first spring app -- Services Spring e Injeção de Dependências (@Service, @Autowired)

---
+++ @KipperDev/CURSO DE SPRING para INICIANTES  - Tutorial Completo de Java Spring/first-spring-app/src/main/java/com/kipper/first_spring_app/configuration/HelloConfiguration.java
@@ -0,0 +1,13 @@
+package com.kipper.first_spring_app.configuration;
+
+import com.sun.jdi.connect.Transport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloConfiguration {
+    @Bean
+    public String helloWorld(){
+        return "Hello World!";
+    }
+}
--- @KipperDev/CURSO DE SPRING para INICIANTES  - Tutorial Completo de Java Spring/first-spring-app/src/main/java/com/kipper/first_spring_app/controller/HelloWorldController.java
+++ @KipperDev/CURSO DE SPRING para INICIANTES  - Tutorial Completo de Java Spring/first-spring-app/src/main/java/com/kipper/first_spring_app/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.kipper.first_spring_app.controller;
 
+import com.kipper.first_spring_app.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 // STATEFULL -> o estado de cada cliente é mantido no servidor
 @RequestMapping("/hello-world")
 public class HelloWorldController {
+    @Autowired
+    private HelloWorldService helloWorldService;
+
+//    public HelloWorldController(HelloWorldService helloWorldService){
+//        this.helloWorldService = helloWorldService;
+//    }
+
     // post, get, delete, put, patch, options, head
     // GET /hello-world/get
     @GetMapping
     public String helloWorld(){
-        return "Hello Video!";
+        return helloWorldService.helloWorld("Kipper");
     }
 
 }

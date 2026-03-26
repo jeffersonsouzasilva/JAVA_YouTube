9348780f1553d8a050e78b0e33c3a2813f4ab205
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-03-26 16:12:41 -0300

    Canal @souzaadev -- spring boot essencials - Criando o @Service

---
+++ @souzaadev/Curso Spring Boot Criando sua primeira API REST do ZERO/spring-boot-essencials/src/main/java/br/com/souza/spring_boot_essencials/service/ProdutoService.java
@@ -0,0 +1,35 @@
+package br.com.souza.spring_boot_essencials.service;
+
+import br.com.souza.spring_boot_essencials.database.model.ProdutoEntity;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    private static final List<ProdutoEntity> PRODUTOS = List.of(
+            ProdutoEntity.builder()
+                    .id(1)
+                    .nome("Notebook")
+                    .preco(new BigDecimal(5000))
+                    .quantidade(10)
+                    .build(),
+            ProdutoEntity.builder()
+                    .id(2)
+                    .nome("Iphone")
+                    .preco(new BigDecimal(7000))
+                    .quantidade(10)
+                    .build(),
+            ProdutoEntity.builder()
+                    .id(3)
+                    .nome("Mouse")
+                    .preco(new BigDecimal(500))
+                    .quantidade(10)
+                    .build()
+    );
+
+    
+}

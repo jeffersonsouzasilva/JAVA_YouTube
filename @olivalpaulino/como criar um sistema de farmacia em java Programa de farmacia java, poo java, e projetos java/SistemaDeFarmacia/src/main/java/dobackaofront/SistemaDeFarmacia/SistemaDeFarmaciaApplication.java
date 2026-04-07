31efed21043c6ba90b4919e672c6c7377dbc0dd4
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-04-07 15:57:12 -0300

    @olivalpaulino -- Sistema de Farmacia - Inicio do Projeto

---
+++ @olivalpaulino/como criar um sistema de farmacia em java Programa de farmacia java, poo java, e projetos java/SistemaDeFarmacia/src/main/java/dobackaofront/SistemaDeFarmacia/SistemaDeFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package dobackaofront.SistemaDeFarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeFarmaciaApplication.class, args);
+	}
+
+}
---
+++ @olivalpaulino/como criar um sistema de farmacia em java Programa de farmacia java, poo java, e projetos java/SistemaDeFarmacia/src/test/java/dobackaofront/SistemaDeFarmacia/SistemaDeFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package dobackaofront.SistemaDeFarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

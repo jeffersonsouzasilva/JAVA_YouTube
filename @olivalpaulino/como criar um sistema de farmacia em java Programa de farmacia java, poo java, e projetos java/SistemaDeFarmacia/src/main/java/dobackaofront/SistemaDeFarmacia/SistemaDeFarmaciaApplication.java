52823205ec3408b60aa2813410c1462969a4c68f
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-04-10 16:35:58 -0300

    @olivalpaulino -- Sistema de Farmacia - Classe BancoDeDados > cadastrar

--- @olivalpaulino/como criar um sistema de farmacia em java Programa de farmacia java, poo java, e projetos java/SistemaDeFarmacia/src/main/java/dobackaofront/SistemaDeFarmacia/SistemaDeFarmaciaApplication.java
+++ @olivalpaulino/como criar um sistema de farmacia em java Programa de farmacia java, poo java, e projetos java/SistemaDeFarmacia/src/main/java/dobackaofront/SistemaDeFarmacia/SistemaDeFarmaciaApplication.java
@@ -1,5 +1,7 @@
 package dobackaofront.SistemaDeFarmacia;
 
+import dobackaofront.SistemaDeFarmacia.controller.BancoDeDados;
+import dobackaofront.SistemaDeFarmacia.model.Item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SistemaDeFarmaciaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaDeFarmaciaApplication.class, args);
-		//System.out.println("Sistema de Farmacia !");
-	}
+		//SpringApplication.run(SistemaDeFarmaciaApplication.class, args);
+		System.out.println("Sistema de Farmacia !");
 
+		Item dipirona = new Item("Dipirona 500mg",100,"Caixa");
+		Item bromoprida = new Item("Bromoprida 200mg",50,"Caixa");
+
+		BancoDeDados banco = new BancoDeDados();
+		banco.cadastrar(dipirona);
+		banco.cadastrar(bromoprida);
+
+	}
 }
--- @olivalpaulino/como criar um sistema de farmacia em java Programa de farmacia java, poo java, e projetos java/SistemaDeFarmacia/src/main/java/dobackaofront/SistemaDeFarmacia/controller/BancoDeDados.java
+++ @olivalpaulino/como criar um sistema de farmacia em java Programa de farmacia java, poo java, e projetos java/SistemaDeFarmacia/src/main/java/dobackaofront/SistemaDeFarmacia/controller/BancoDeDados.java
@@ -2,10 +2,36 @@ package dobackaofront.SistemaDeFarmacia.controller;
 
 import dobackaofront.SistemaDeFarmacia.model.Item;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
 public class BancoDeDados {
     private Item item;
     public BancoDeDados(){}
-    public void cadastrar(Item item){}
+    public void cadastrar(Item item){
+        try {
+            OutputStream os = new FileOutputStream("medicamentos.txt",true);
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+            String linha = item.getNome()+","+item.getQuantidade()+","+item.getTipo();
+
+            bw.write(linha);
+            bw.newLine();
+
+            bw.close();
+            osw.close();
+            os.close();
+
+            System.out.println("O medicamento "+item.getNome()+" foi cadastrado com sucesso!");
+        }catch (Exception e){
+            System.out.println("Não conseguiu cadastrar o medicamento!");
+            System.out.println(e);
+        }
+
+    }
     public void editar(int codigo){}
     public void pesquisar(int codigo){}
     public void excluir(int codigo){}

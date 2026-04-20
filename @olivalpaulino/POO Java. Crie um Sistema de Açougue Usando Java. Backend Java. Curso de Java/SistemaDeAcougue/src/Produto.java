86a540628e776f1daf22ec60d472ec2bde94fd07
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-04-20 18:40:57 -0300

    @olivalpaulino -- Sistema de Açougue - Classe Produto > Atributos, Construtor e Metodos

--- @olivalpaulino/POO Java. Crie um Sistema de Açougue Usando Java. Backend Java. Curso de Java/SistemaDeAcougue/src/Produto.java
+++ @olivalpaulino/POO Java. Crie um Sistema de Açougue Usando Java. Backend Java. Curso de Java/SistemaDeAcougue/src/Produto.java
@@ -1,3 +1,41 @@
+/*
+ - Para gerar Getter & Setter, clickar com direito, opção generate e escolher getter e setter
+ - Para emitir toString digite to e escolha a opção valores generate
+*/
 public class Produto {
     //Atributos
+    private String nome;
+    private double precoPorKg;
+
+    //construtor
+    public Produto(String nome, double precoPorKg){
+        this.nome = nome;
+        this.precoPorKg = precoPorKg;
+    }
+
+    //metodos get e set
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPrecoPorKg() {
+        return precoPorKg;
+    }
+
+    public void setPrecoPorKg(double precoPorKg) {
+        this.precoPorKg = precoPorKg;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", precoPorKg=" + precoPorKg +
+                '}';
+    }
+    
 }

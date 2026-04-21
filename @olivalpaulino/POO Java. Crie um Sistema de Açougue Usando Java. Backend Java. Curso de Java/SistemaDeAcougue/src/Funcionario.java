9d26ccb8e2059c8088798e51697f00a3024e2aed
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-04-21 09:10:00 -0300

    @olivalpaulino -- Sistema de Açougue - Classe Funcionario > Atributos, Construtor e Metodos

--- @olivalpaulino/POO Java. Crie um Sistema de Açougue Usando Java. Backend Java. Curso de Java/SistemaDeAcougue/src/Funcionario.java
+++ @olivalpaulino/POO Java. Crie um Sistema de Açougue Usando Java. Backend Java. Curso de Java/SistemaDeAcougue/src/Funcionario.java
@@ -1,2 +1,41 @@
 public class Funcionario {
+    //Atributos
+    protected  String nome;
+    protected  double salario;
+
+    //Construtor
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    //Metodos get e set
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+
+    //Metodo especifico de classe
+    public void exibirDados(){
+        System.out.println(toString());
+    }
 }
--- @olivalpaulino/POO Java. Crie um Sistema de Açougue Usando Java. Backend Java. Curso de Java/SistemaDeAcougue/src/Produto.java
+++ @olivalpaulino/POO Java. Crie um Sistema de Açougue Usando Java. Backend Java. Curso de Java/SistemaDeAcougue/src/Produto.java
@@ -37,5 +37,5 @@ public class Produto {
                 ", precoPorKg=" + precoPorKg +
                 '}';
     }
-    
+
 }

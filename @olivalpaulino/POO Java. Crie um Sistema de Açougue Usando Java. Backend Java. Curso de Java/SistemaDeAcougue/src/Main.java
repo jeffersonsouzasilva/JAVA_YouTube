7a3f143e1fc81976f77e9bad18d163b7579bff9b
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-04-26 19:43:08 -0300

    @olivalpaulino -- Sistema de Açougue - Main > Criando Produto, funcionario, Pedido e Exibir dados

--- @olivalpaulino/POO Java. Crie um Sistema de Açougue Usando Java. Backend Java. Curso de Java/SistemaDeAcougue/src/Main.java
+++ @olivalpaulino/POO Java. Crie um Sistema de Açougue Usando Java. Backend Java. Curso de Java/SistemaDeAcougue/src/Main.java
@@ -1,7 +1,25 @@
 
 public class Main {
     public static void main(String[] args) {
+        //Criando Produtos
+        Produto picanha = new Produto("Picanha",79.90);
+        Produto costela = new Produto("Costela", 29.90);
 
+        //Criando funcionario
+        Atendente atendente = new Atendente("Carlos", 1800);
+        Acougueiro acougueiro = new Acougueiro("José", 2500);
 
+        //Criando Pedido
+        Pedido pedido = new Pedido();
+        pedido.adicionarProduto(picanha,1.5);
+        pedido.adicionarProduto(costela, 2);
+
+        //Exibir dados
+        System.out.println("Dados dos Funcionarios --------------");
+        atendente.exibirDados();
+        acougueiro.exibirDados();
+
+        System.out.println(" ");
+        pedido.exibirPedido();
     }
 }

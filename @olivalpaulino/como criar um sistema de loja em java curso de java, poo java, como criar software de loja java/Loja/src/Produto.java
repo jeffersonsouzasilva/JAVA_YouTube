a37a33017964cad015f91612e20bbeec63582cb5
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-04-28 12:27:23 -0300

    @olivalpaulino -- Sistema de Loja - Classe Produto > Atributos, Constructor, Getter e Setter

--- @olivalpaulino/como criar um sistema de loja em java curso de java, poo java, como criar software de loja java/Loja/src/Produto.java
+++ @olivalpaulino/como criar um sistema de loja em java curso de java, poo java, como criar software de loja java/Loja/src/Produto.java
@@ -1,3 +1,41 @@
 public class Produto {
+    private String nome;
+    private String descricao;
+    private double preco;
 
+    public Produto(){
+        this.nome = "";
+        this.descricao = "";
+        this.preco = 0;
+    }
+
+    public Produto(String nome, String descricao, double preco) {
+        this.setNome(nome);
+        this.setDescricao(descricao);
+        this.setPreco(preco);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 }

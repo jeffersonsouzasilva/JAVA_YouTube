eb56084dbc64b60ebbef9d622e741e0dac335c67
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-04-28 13:52:40 -0300

    @olivalpaulino -- Sistema de Loja - Classe Main > adicionando dados aos atributos da Classe Produto | Classe Carrinho > adicionar e remover produto

--- @olivalpaulino/como criar um sistema de loja em java curso de java, poo java, como criar software de loja java/Loja/src/Carrinho.java
+++ @olivalpaulino/como criar um sistema de loja em java curso de java, poo java, como criar software de loja java/Loja/src/Carrinho.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Carrinho {
+    ArrayList<Produto> produtos;
+    public Carrinho(){
+        this.produtos = new ArrayList<>();
+    }
+
+    public void adicionar(Produto p){
+        produtos.add(p);
+        System.out.println("Produto "+p.getNome()+" foi adicionado no carrinho!");
+    }
+
+    public String remover(String nome){
+        int codigo = -1;
+        for (int i=0; i<produtos.size(); i++){
+            if (produtos.get(i).equals(nome)){
+                codigo = i;
+                break;
+            } else {
+                continue;
+            }
+        }
+
+        if (codigo == +1){
+            return null;
+        } else {
+            return "O produto "+nome+" foi removido do carrinho!";
+        }
+    }
 }
--- @olivalpaulino/como criar um sistema de loja em java curso de java, poo java, como criar software de loja java/Loja/src/Main.java
+++ @olivalpaulino/como criar um sistema de loja em java curso de java, poo java, como criar software de loja java/Loja/src/Main.java
@@ -1,6 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
+        Produto p1 = new Produto("Chaveiro Seja Forte e Corajosa","Madeira, cordão, adesivo",15.90);
+        Produto p2 = new Produto("Terço Mães que Oram pelos Filhos","cordão, metal e adesivo resinado",32.90);
+
         System.out.println("Loja Javeiros");
     }
 }

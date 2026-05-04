29940410a11bc3e04842f564951b3d5cf79178f0
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-05-04 12:12:50 -0300

    @olivalpaulino -- Sistema de Escola - Classe Pessoa > Cronstrctor, toString, Getter e Setter

--- @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Aluno.java
+++ @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Aluno.java
@@ -1,4 +1,13 @@
 package model;
 
-public class Aluno {
+import java.util.ArrayList;
+
+public class Aluno extends Pessoa{
+    private Turma turma;
+    private ArrayList<Disciplina> disciplinas;
+
+    public Aluno(Turma turma, ArrayList<Disciplina> disciplinas) {
+        this.turma = turma;
+        this.disciplinas = disciplinas;
+    }
 }
--- @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Pessoa.java
+++ @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Pessoa.java
@@ -1,4 +1,35 @@
 package model;
 
-public class Pessoa {
+public abstract class Pessoa {
+    private String nome;
+    private String id;
+
+    public Pessoa(String nome, String id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
 }

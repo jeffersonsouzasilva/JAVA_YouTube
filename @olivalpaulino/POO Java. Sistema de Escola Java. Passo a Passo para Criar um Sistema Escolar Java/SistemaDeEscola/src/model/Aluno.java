ee14fe50fdd91f60fb6c859c549d77967d1f5d7e
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-05-04 15:00:15 -0300

    @olivalpaulino -- Sistema de Escola - Classe Aluno > Cronstructor

--- @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Aluno.java
+++ @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Aluno.java
@@ -6,7 +6,8 @@ public class Aluno extends Pessoa{
     private Turma turma;
     private ArrayList<Disciplina> disciplinas;
 
-    public Aluno(Turma turma, ArrayList<Disciplina> disciplinas) {
+    public Aluno(String nome, String id, Turma turma, ArrayList<Disciplina> disciplinas) {
+        super(nome, id);
         this.turma = turma;
         this.disciplinas = disciplinas;
     }

8825c661a11a17be7cc973f701daddbf8fa1fb9b
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-05-07 14:40:08 -0300

    @olivalpaulino -- Sistema de Escola - Classe aluno > toString, Getter e Setter

--- @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Professor.java
+++ @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Professor.java
@@ -6,7 +6,9 @@ public class Professor extends Pessoa{
     private ArrayList<Turma> turmas;
     private Disciplina disciplina;
 
-    public Professor(String nome, String id, ArrayList<Turma> turmas, Disciplina disciplina) {
+    public Professor(String nome, String id,
+                     ArrayList<Turma> turmas,
+                     Disciplina disciplina) {
         super(nome, id);
         this.turmas = turmas;
         this.disciplina = disciplina;

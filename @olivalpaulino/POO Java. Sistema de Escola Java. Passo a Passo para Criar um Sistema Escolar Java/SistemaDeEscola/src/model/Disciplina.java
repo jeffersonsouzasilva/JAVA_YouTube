e431f2801f63d12c461ce45882dfdff8976c8f42
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-05-08 12:04:23 -0300

    @olivalpaulino -- Sistema de Escola - Classe Disciplina > Constructor, Getter e Setter

--- @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Disciplina.java
+++ @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Disciplina.java
@@ -3,50 +3,50 @@ package model;
 import java.util.ArrayList;
 
 public class Disciplina {
-//    private String codigo;
-//    private ArrayList<Aluno> alunos;
-//    private Professor professor;
-//    private ArrayList<Caderneta> cadernetas;
-//
-//    public Disciplina(String codigo, ArrayList<Aluno> alunos,
-//                      Professor professor,
-//                      ArrayList<Caderneta> cadernetas) {
-//        this.codigo = codigo;
-//        this.alunos = alunos;
-//        this.professor = professor;
-//        this.cadernetas = cadernetas;
-//    }
-//
-//    public String getCodigo() {
-//        return codigo;
-//    }
-//
-//    public void setCodigo(String codigo) {
-//        this.codigo = codigo;
-//    }
-//
-//    public ArrayList<Aluno> getAlunos() {
-//        return alunos;
-//    }
-//
-//    public void setAlunos(ArrayList<Aluno> alunos) {
-//        this.alunos = alunos;
-//    }
-//
-//    public Professor getProfessor() {
-//        return professor;
-//    }
-//
-//    public void setProfessor(Professor professor) {
-//        this.professor = professor;
-//    }
-//
-//    public ArrayList<Caderneta> getCadernetas() {
-//        return cadernetas;
-//    }
-//
-//    public void setCadernetas(ArrayList<Caderneta> cadernetas) {
-//        this.cadernetas = cadernetas;
-//    }
+    private String codigo;
+    private ArrayList<Aluno> alunos;
+    private Professor professor;
+    private ArrayList<Caderneta> cadernetas;
+
+    public Disciplina(String codigo, ArrayList<Aluno> alunos,
+                      Professor professor,
+                      ArrayList<Caderneta> cadernetas) {
+        this.codigo = codigo;
+        this.alunos = alunos;
+        this.professor = professor;
+        this.cadernetas = cadernetas;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public ArrayList<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(ArrayList<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public ArrayList<Caderneta> getCadernetas() {
+        return cadernetas;
+    }
+
+    public void setCadernetas(ArrayList<Caderneta> cadernetas) {
+        this.cadernetas = cadernetas;
+    }
 
 }

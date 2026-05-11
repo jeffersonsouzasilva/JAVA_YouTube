0ff07a71aa7c1861a9f507580e3e1f9dfe9293a3
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-05-11 15:04:20 -0300

    @olivalpaulino -- Sistema de Escola - Classe Turma > Atributos

--- @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Turma.java
+++ @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Turma.java
@@ -1,4 +1,12 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Turma {
+    private String nome;
+    private String codigo;
+    private ArrayList<Disciplina> disciplinas;
+    private ArrayList<Professor> professores;
+    private ArrayList<Aluno> alunos;
+    
 }

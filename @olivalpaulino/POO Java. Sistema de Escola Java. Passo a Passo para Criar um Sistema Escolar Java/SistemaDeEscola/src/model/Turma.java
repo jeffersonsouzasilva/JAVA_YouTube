634c2d41e6047d05da1a95258aa02687b98ab4ba
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-05-12 15:51:07 -0300

    @olivalpaulino -- Sistema de Escola - Classe Turma > Constructor, toString, Getter e Setter

--- @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Turma.java
+++ @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Turma.java
@@ -8,5 +8,66 @@ public class Turma {
     private ArrayList<Disciplina> disciplinas;
     private ArrayList<Professor> professores;
     private ArrayList<Aluno> alunos;
-    
+
+    public Turma(String nome, String codigo,
+                 ArrayList<Disciplina> disciplinas,
+                 ArrayList<Professor> professores,
+                 ArrayList<Aluno> alunos) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.disciplinas = disciplinas;
+        this.professores = professores;
+        this.alunos = alunos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public ArrayList<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(ArrayList<Disciplina> disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+
+    public ArrayList<Professor> getProfessores() {
+        return professores;
+    }
+
+    public void setProfessores(ArrayList<Professor> professores) {
+        this.professores = professores;
+    }
+
+    public ArrayList<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(ArrayList<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
+    @Override
+    public String toString() {
+        return "Turma{" +
+                "nome='" + nome + '\'' +
+                ", codigo='" + codigo + '\'' +
+                ", disciplinas=" + disciplinas +
+                ", professores=" + professores +
+                ", alunos=" + alunos +
+                '}';
+    }
 }

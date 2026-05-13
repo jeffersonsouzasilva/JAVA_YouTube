0c71461bc9bbba743dc45132eac3c9b4080c559b
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-05-13 15:31:56 -0300

    @olivalpaulino -- Sistema de Escola - Classe Escola > Atributos

--- @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Escola.java
+++ @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Escola.java
@@ -1,4 +1,11 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Escola {
+    private ArrayList<Turma> turmas;
+    private ArrayList<Aluno> alunos;
+    private ArrayList<Professor> professores;
+    private ArrayList<Disciplina> disciplinas;
+    private ArrayList<Caderneta> cadernetas;
 }

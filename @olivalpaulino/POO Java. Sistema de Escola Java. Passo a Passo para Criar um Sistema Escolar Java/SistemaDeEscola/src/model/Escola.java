586c84300ef915f6ac5c0bbf7bb4929ec8f79c31
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-05-14 11:17:11 -0300

    @olivalpaulino -- Sistema de Escola - Classe Escola > Constructor

--- @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Escola.java
+++ @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Escola.java
@@ -3,9 +3,26 @@ package model;
 import java.util.ArrayList;
 
 public class Escola {
+    private String nome;
+    private String codigo;
     private ArrayList<Turma> turmas;
     private ArrayList<Aluno> alunos;
     private ArrayList<Professor> professores;
     private ArrayList<Disciplina> disciplinas;
     private ArrayList<Caderneta> cadernetas;
+
+    public Escola(String nome, String codigo,
+                  ArrayList<Turma> turmas,
+                  ArrayList<Aluno> alunos,
+                  ArrayList<Professor> professores,
+                  ArrayList<Disciplina> disciplinas,
+                  ArrayList<Caderneta> cadernetas) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.turmas = turmas;
+        this.alunos = alunos;
+        this.professores = professores;
+        this.disciplinas = disciplinas;
+        this.cadernetas = cadernetas;
+    }
 }

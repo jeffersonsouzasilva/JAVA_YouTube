aa7025411aa08dfb48b69f9761cf89dbd03330ce
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-05-15 15:02:33 -0300

    @olivalpaulino -- Sistema de Escola - Classe Escola > Getter e Setter

--- @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Escola.java
+++ @olivalpaulino/POO Java. Sistema de Escola Java. Passo a Passo para Criar um Sistema Escolar Java/SistemaDeEscola/src/model/Escola.java
@@ -25,4 +25,60 @@ public class Escola {
         this.disciplinas = disciplinas;
         this.cadernetas = cadernetas;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public ArrayList<Turma> getTurmas() {
+        return turmas;
+    }
+
+    public void setTurmas(ArrayList<Turma> turmas) {
+        this.turmas = turmas;
+    }
+
+    public ArrayList<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(ArrayList<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
+    public ArrayList<Professor> getProfessores() {
+        return professores;
+    }
+
+    public void setProfessores(ArrayList<Professor> professores) {
+        this.professores = professores;
+    }
+
+    public ArrayList<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(ArrayList<Disciplina> disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+
+    public ArrayList<Caderneta> getCadernetas() {
+        return cadernetas;
+    }
+
+    public void setCadernetas(ArrayList<Caderneta> cadernetas) {
+        this.cadernetas = cadernetas;
+    }
 }

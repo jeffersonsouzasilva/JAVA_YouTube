a4e195089eb8ee68822854e2e989b88220cfecf4
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-06-03 17:54:49 -0300

    @matheuslf -- Projeto OrientacaoObjetos - Classe Volante > Atributos, Constructor, Getter e Setter

--- @matheuslf/PROGRAMAÇÃO ORIENTADA A OBJETOS EM POUCOS MINUTOS/OrientacaoObjetos/src/Volante.java
+++ @matheuslf/PROGRAMAÇÃO ORIENTADA A OBJETOS EM POUCOS MINUTOS/OrientacaoObjetos/src/Volante.java
@@ -1,2 +1,25 @@
 public class Volante {
+    private String tipo;
+    private boolean possuiControles;
+
+    public Volante(String tipo, boolean possuiControles) {
+        this.tipo = tipo;
+        this.possuiControles = possuiControles;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public boolean isPossuiControles() {
+        return possuiControles;
+    }
+
+    public void setPossuiControles(boolean possuiControles) {
+        this.possuiControles = possuiControles;
+    }
 }

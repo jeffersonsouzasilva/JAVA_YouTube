bb43137cabeb09a9b1db5418355c3c5484eadc5d
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-06-08 13:04:24 -0300

    @matheuslf -- Projeto OrientacaoObjetos - Bateria > Atributos, Constructor, Getter e Setter

--- @matheuslf/PROGRAMAÇÃO ORIENTADA A OBJETOS EM POUCOS MINUTOS/OrientacaoObjetos/src/Bateria.java
+++ @matheuslf/PROGRAMAÇÃO ORIENTADA A OBJETOS EM POUCOS MINUTOS/OrientacaoObjetos/src/Bateria.java
@@ -1,4 +1,26 @@
 public class Bateria {
 
+    private int capacidade;
+    private int nivelCarga;
 
+    public Bateria(int capacidade, int nivelCarga) {
+        this.capacidade = capacidade;
+        this.nivelCarga = nivelCarga;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public int getNivelCarga() {
+        return nivelCarga;
+    }
+
+    public void setNivelCarga(int nivelCarga) {
+        this.nivelCarga = nivelCarga;
+    }
 }

ba22ac534664a3adfc9ba253fddbae6b3f0fba66
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-06-10 15:06:03 -0300

    @matheuslf -- Projeto OrientacaoObjetos - CarroEletrico > Atributos, Constructor e Override acelerar

--- @matheuslf/PROGRAMAÇÃO ORIENTADA A OBJETOS EM POUCOS MINUTOS/OrientacaoObjetos/src/CarroEletrico.java
+++ @matheuslf/PROGRAMAÇÃO ORIENTADA A OBJETOS EM POUCOS MINUTOS/OrientacaoObjetos/src/CarroEletrico.java
@@ -1,3 +1,15 @@
-public class CarroEletrico {
+public class CarroEletrico extends Carro{
 
+    private Bateria bateria;
+
+    public CarroEletrico(String marca, String modelo, int ano, double velocidade, Volante volante, Pedais pedais, Bateria bateria) {
+        super(marca, modelo, ano, velocidade, volante, pedais);
+        this.bateria = bateria;
+    }
+
+    @Override
+    public void acelerar(double incremento) {
+        //bateria.consumir(2);
+        super.acelerar(incremento);
+    }
 }

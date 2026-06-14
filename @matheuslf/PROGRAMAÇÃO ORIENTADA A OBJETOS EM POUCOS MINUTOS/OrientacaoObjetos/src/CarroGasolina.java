55f75dfbc3e6a69370b0805f6b906b95609f8a57
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-06-14 15:28:22 -0300

    @matheuslf -- Projeto OrientacaoObjetos - CarroGasolina > Atributos e Override acelerar

--- @matheuslf/PROGRAMAÇÃO ORIENTADA A OBJETOS EM POUCOS MINUTOS/OrientacaoObjetos/src/CarroGasolina.java
+++ @matheuslf/PROGRAMAÇÃO ORIENTADA A OBJETOS EM POUCOS MINUTOS/OrientacaoObjetos/src/CarroGasolina.java
@@ -1,3 +1,10 @@
 public class CarroGasolina extends Carro{
+    private double nivelCombustivel;
 
+    @Override
+    public void acelerar(double incremento) {
+        setVelocidade(getVelocidade() + incremento);
+        nivelCombustivel -= 0.5;
+        super.acelerar(incremento);
+    }
 }

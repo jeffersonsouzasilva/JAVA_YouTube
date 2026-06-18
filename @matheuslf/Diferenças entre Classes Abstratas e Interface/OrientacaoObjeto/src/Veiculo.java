0fa0dfff53bdac2765b7409aebd25be419a910ca
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-06-18 12:37:46 -0300

    @matheuslf -- Projeto OrientacaoObjeto - Classe Veiculo > Constructor

--- @matheuslf/Diferenças entre Classes Abstratas e Interface/OrientacaoObjeto/src/Veiculo.java
+++ @matheuslf/Diferenças entre Classes Abstratas e Interface/OrientacaoObjeto/src/Veiculo.java
@@ -2,5 +2,10 @@ public abstract class Veiculo {
 
     protected String modelo;
     protected int velocidade;
+
+    public Veiculo(String modelo, int velocidade) {
+        this.modelo = modelo;
+        this.velocidade = velocidade;
+    }
     
 }

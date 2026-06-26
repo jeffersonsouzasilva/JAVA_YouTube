858ee2cbb673612455c1c056516e53db0ed00d1f
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-06-26 14:54:54 -0300

    @matheuslf -- Projeto OrientacaoObjeto - Classe Carro > extends Veiculo, Constructor e Metodo frear

--- @matheuslf/Diferenças entre Classes Abstratas e Interface/OrientacaoObjeto/src/Carro.java
+++ @matheuslf/Diferenças entre Classes Abstratas e Interface/OrientacaoObjeto/src/Carro.java
@@ -1,3 +1,11 @@
-public class Carro {
-    
+public class Carro extends Veiculo {
+
+    public Carro(String modelo) {
+        super(modelo, 0);
+    }
+
+    @Override
+    public void frear() {
+        
+    }
 }

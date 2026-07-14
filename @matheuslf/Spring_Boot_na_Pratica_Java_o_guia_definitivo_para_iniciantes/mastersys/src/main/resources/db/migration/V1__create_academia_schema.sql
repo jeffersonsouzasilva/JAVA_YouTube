3e057b7e26e2541a26eb1d9dd7b59b3a57e8c2b2
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-07-14 17:41:08 -0300

    @matheuslf - Projeto mastersys -- V1 create academia schema > CREATE TABLE alunos, modalidades, graduacao e planos

---
+++ @matheuslf/Spring_Boot_na_Pratica_Java_o_guia_definitivo_para_iniciantes/mastersys/src/main/resources/db/migration/V1__create_academia_schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE alunos(
+    id BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    data_nascimento DATE,
+    sexo VARCHAR(1) CHECK (sexo IN('M','F')),
+    telefone VARCHAR(30),
+    celular VARCHAR(30),
+    email VARCHAR(150),
+    observacao TEXT,
+    endereco VARCHAR(150),
+    numero VARCHAR(20),
+    complemento VARCHAR(100),
+    bairro VARCHAR(100),
+    cidade VARCHAR(100),
+    estado VARCHAR(20),
+    cep VARCHAR(20),
+    criando_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    atualizado_em TIMESTAMP
+);
+
+CREATE TABLE modalidades(
+    id BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL UNIQUE,
+    ativo BOOLEAN NOT DEFAULT TRUE
+);
+
+CREATE TABLE graduacao(
+    id BIGSERIAL PRIMARY KEY,
+    modalidade_id BIGINT NOT NULL REFERENCES modalidades(id),
+    nome VARCHAR(100) NOT NULL,
+    UNIQUE (modalidade_id,nome)
+);
+
+CREATE TABLE planos(
+    id BIGSERIAL PRIMARY KEY,
+    modalidade_id BIGINT NOT NULL REFERENCES modalidades(id),
+    nome VARCHAR(100) NOT NULL,
+    valor_mensal NUMERIC(10,2) NOT NULL CHECK(valor_mensal >= 0),
+    ativo BOOLEAN NOT NULL DEFAULT TRUE,
+    UNIQUE (modalidade_id, nome)
+);

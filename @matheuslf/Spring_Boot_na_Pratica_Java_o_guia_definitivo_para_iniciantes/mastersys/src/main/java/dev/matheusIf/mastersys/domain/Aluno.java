7fee81e1bfc8e59d58a34cdcdf7c9b94cf965ae8
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-08-04 10:05:31 -0300

    @matheuslf - Projeto mastersys -- Aluno > String e @Column

--- @matheuslf/Spring_Boot_na_Pratica_Java_o_guia_definitivo_para_iniciantes/mastersys/src/main/java/dev/matheusIf/mastersys/domain/Aluno.java
+++ @matheuslf/Spring_Boot_na_Pratica_Java_o_guia_definitivo_para_iniciantes/mastersys/src/main/java/dev/matheusIf/mastersys/domain/Aluno.java
@@ -2,11 +2,18 @@ package dev.matheusIf.mastersys.domain;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "alunos")
 public class Aluno {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String nome;
+
+    @Column(name = "data_nascimento")
+    private LocalDate dataNascimento;
 
 }

10fc9ce13e8fd55a732d4cd5db5e0e1d9fc5da07
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-08-14 16:07:51 -0300

    @matheuslf - Projeto mastersys -- Modalidade > @Entity e @Table

--- @matheuslf/Spring_Boot_na_Pratica_Java_o_guia_definitivo_para_iniciantes/mastersys/src/main/java/dev/matheusIf/mastersys/domain/Modalidade.java
+++ @matheuslf/Spring_Boot_na_Pratica_Java_o_guia_definitivo_para_iniciantes/mastersys/src/main/java/dev/matheusIf/mastersys/domain/Modalidade.java
@@ -1,4 +1,9 @@
 package dev.matheusIf.mastersys.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "modalidades")
 public class Modalidade {
 }

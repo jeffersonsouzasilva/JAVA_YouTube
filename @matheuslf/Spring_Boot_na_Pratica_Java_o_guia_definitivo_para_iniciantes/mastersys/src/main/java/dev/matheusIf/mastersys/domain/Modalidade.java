397a0f54f83d4b62b34f4d03c5261d6bb7b471f2
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-08-19 16:06:45 -0300

    @matheuslf - Projeto mastersys -- Modalidade > Getter e Setter

--- @matheuslf/Spring_Boot_na_Pratica_Java_o_guia_definitivo_para_iniciantes/mastersys/src/main/java/dev/matheusIf/mastersys/domain/Modalidade.java
+++ @matheuslf/Spring_Boot_na_Pratica_Java_o_guia_definitivo_para_iniciantes/mastersys/src/main/java/dev/matheusIf/mastersys/domain/Modalidade.java
@@ -12,4 +12,28 @@ public class Modalidade {
     private String nome;
 
     private Boolean ativo = true;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
 }

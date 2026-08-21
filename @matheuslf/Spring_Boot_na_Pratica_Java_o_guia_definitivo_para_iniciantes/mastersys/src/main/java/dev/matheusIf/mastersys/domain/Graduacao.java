bc37f9010d1d18cd659e701916ac8e23649b489f
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-08-21 12:47:13 -0300

    @matheuslf - Projeto mastersys -- Graduacao > @Id e @GeneratedValue

--- @matheuslf/Spring_Boot_na_Pratica_Java_o_guia_definitivo_para_iniciantes/mastersys/src/main/java/dev/matheusIf/mastersys/domain/Graduacao.java
+++ @matheuslf/Spring_Boot_na_Pratica_Java_o_guia_definitivo_para_iniciantes/mastersys/src/main/java/dev/matheusIf/mastersys/domain/Graduacao.java
@@ -1,4 +1,11 @@
 package dev.matheusIf.mastersys.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "graduações")
 public class Graduacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }

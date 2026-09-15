7730220a25104dfa39eed22678b0459b7e4e9673
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-09-15 14:08:36 -0300

    @matheuslf - Projeto mastersys -- Matricula > Atributo Long

--- @matheuslf/Spring_Boot_na_Pratica_Java_o_guia_definitivo_para_iniciantes/mastersys/src/main/java/dev/matheusIf/mastersys/domain/Matricula.java
+++ @matheuslf/Spring_Boot_na_Pratica_Java_o_guia_definitivo_para_iniciantes/mastersys/src/main/java/dev/matheusIf/mastersys/domain/Matricula.java
@@ -1,4 +1,11 @@
 package dev.matheusIf.mastersys.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "matriculas")
 public class Matricula {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }

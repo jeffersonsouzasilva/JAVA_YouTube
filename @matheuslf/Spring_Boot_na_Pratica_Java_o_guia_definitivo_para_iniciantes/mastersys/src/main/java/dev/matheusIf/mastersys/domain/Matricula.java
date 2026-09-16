3e2662b743441f9fec0d19521828734585b34592
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2026-09-16 16:15:46 -0300

    @matheuslf - Projeto mastersys -- Matricula > @Column Atributo dataMatricula

--- @matheuslf/Spring_Boot_na_Pratica_Java_o_guia_definitivo_para_iniciantes/mastersys/src/main/java/dev/matheusIf/mastersys/domain/Matricula.java
+++ @matheuslf/Spring_Boot_na_Pratica_Java_o_guia_definitivo_para_iniciantes/mastersys/src/main/java/dev/matheusIf/mastersys/domain/Matricula.java
@@ -2,10 +2,15 @@ package dev.matheusIf.mastersys.domain;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "matriculas")
 public class Matricula {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "data_matricula")
+    private LocalDate dataMatricula;
 }
